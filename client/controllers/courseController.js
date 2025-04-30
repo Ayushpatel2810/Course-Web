aa28acd4a9f564cf149d1ee4ab315a5ea8a97343
Ayush Patel <119032211+Ayushpatel2810@users.noreply.github.com>
2025-04-30 17:58:55 +0530

    Update courseController.js

--- client/controllers/courseController.js
+++ client/controllers/courseController.js
@@ -172,7 +172,7 @@ app.controller('courseController', function($scope, $http, $window, authService)
     }, {
       headers: { Authorization: `Bearer ${token}` }
     }).then(res => {
-      const stripe = Stripe('pk_test_51QuPUwHW7bdkqoG5HdOmDPIkW3b13MWU2EsMWxgGyCOPtaDDBnDbLfKys7vNvRpj68U6cWCFOx9CuNUfCSEOU0RO00AiX0f2nY');
+      const stripe = Stripe('');
       stripe.redirectToCheckout({ sessionId: res.data.id });
     }).catch(err => {
       console.error('Stripe checkout error:', err);
